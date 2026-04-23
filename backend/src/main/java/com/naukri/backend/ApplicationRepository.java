dc9954303a49a68faf8e98cf83a6710658ce5965
harsimrankaur0910 <harsimrankaur9102005@gmail.com>
2026-04-23 23:35:29 +0530

    Delete backend directory

--- backend/src/main/java/com/naukri/backend/ApplicationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.naukri.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}
--- backend/src/main/java/com/naukri/backend/JobRepository.java
+++
@@ -1,6 +0,0 @@
-package com.naukri.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
